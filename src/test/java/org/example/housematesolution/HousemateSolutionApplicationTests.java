1fc41597cef9a36f847e7a791bbda6e30b987249
ineoaas <justinleomaas@gmail.com>
2026-09-15 11:33:58 +0200

    Implemented Docker with DB to replace h2 test one

--- src/test/java/org/example/housematesolution/HousemateSolutionApplicationTests.java
+++ src/test/java/org/example/housematesolution/HousemateSolutionApplicationTests.java
@@ -2,8 +2,10 @@ package org.example.housematesolution;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestcontainersConfiguration.class)
 class HousemateSolutionApplicationTests {
 
     @Test
