a6ead34b5bd07a36381d8ec06407a3f0d1690670
ineoaas <justinleomaas@gmail.com>
2026-09-15 10:13:01 +0200

    Inital Commit

---
+++ src/main/java/org/example/housematesolution/HousemateSolutionApplication.java
@@ -0,0 +1,13 @@
+package org.example.housematesolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousemateSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HousemateSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/housematesolution/HousemateSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.housematesolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousemateSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
